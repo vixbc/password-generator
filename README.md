3ee15acdf8c55a07c338945719901a5a28b67702
vixbc <92274245+vixbc@users.noreply.github.com>
2022-12-01 07:16:05 +0000

    final draft
    sorry i didn't upload the correct file to the github when submitting the homework

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 this random password generator which allows users to select the type and number of characters to be used.
 
 the generator utilises prompts to get user inputs and displays the final password on a card.
+
+links:
+https://vixbc.github.io/password-generator/
+
+https://github.com/vixbc/password-generator
--- script.js
+++ script.js
@@ -26,10 +26,10 @@ var specialCharacters = [
 ];
 
 // Array of numeric characters to be included in password
-var charNum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
-var charLow = [
+var lowerCasedCharacters = [
   'a',
   'b',
   'c',
@@ -59,7 +59,7 @@ var charLow = [
 ];
 
 // Array of uppercase characters to be included in password
-var charUp = [
+var upperCasedCharacters = [
   'A',
   'B',
   'C',
@@ -88,20 +88,150 @@ var charUp = [
   'Z'
 ];
 
+
+
+
+// password option list
+var passOptions = {
+  len: 0,
+  charSpec: false,
+  charNum: false,
+  charLow: false,
+  charUp: false,
+  types:0
+}
+
+//functions to populate option list
+function userLength() {
+  var userLenOption = 0;
+  userLenOption = Number(prompt("how long should your pasword be?(10-64)"));
+  if(userLenOption >= 10 && userLenOption <= 64){
+    passOptions.len = userLenOption;
+  } else {
+    alert ("please make sure you enter a number between 10 and 64")
+    userLength()
+  }
+  return userLenOption
+ }
+
+function userCharUp() {
+  var userCharUpOption = false;userCharUpOption = prompt("do you want uppercase characters in your password? [y/n]")
+  if(userCharUpOption == "y" || userCharUpOption == "Y"){
+    passOptions.charUp = true
+    passOptions.types = +1
+  } else if(userCharUpOption == "n" || userCharUpOption == "N") {
+    delete passOptions.charUp
+  } else {
+    alert ("please us y or n only to indicate your choice")
+    userCharUp()
+  }
+  return userCharUpOption
+  }
+
+
+function userCharLow() {
+  var userCharLowOption = false;
+  userCharLowOption = prompt("do you want lowercase characters in your password? [y/n]")
+  if(userCharLowOption == "y" || userCharLowOption == "Y"){
+    passOptions.charLow = true
+    passOptions.types = +1
+    } else if(userCharLowOption == "n" || userCharLowOption == "N") {
+      delete passOptions.charLow
+    } else {
+      alert ("please us y or n only to indicate your choice")
+      userCharLow()
+    }
+    return userCharLowOption
+    }
+  
+  
+ function userCharNum() {
+  var userCharNumOption = false;
+  userCharNumOption = prompt("do you want numeric characters in your password? [y/n]")
+  if(userCharNumOption == "y" || userCharNumOption == "Y"){
+    passOptions.charNum = true
+    passOptions.types = +1
+  } else if(userCharNumOption == "n" || userCharNumOption == "N") {
+    delete passOptions.charNum
+  } else {
+    alert ("please us y or n only to indicate your choice")
+    userCharUp()
+  }
+  return userCharNumOption
+  }
+
+
+  function userCharSpec() {
+    var userCharSpecOption = false;
+    userCharSpecOption = prompt("do you want uppercase characters in your password? [y/n]")
+    if(userCharSpecOption == "y" || userCharSpecOption == "Y"){
+      passOptions.charSpec = true
+      passOptions.types = +1
+    } else if(userCharSpecOption == "n" || userCharSpecOption == "N") {
+      delete passOptions.charSpec
+    } else {
+      alert ("please us y or n only to indicate your choice")
+      userCharUp()
+    }
+    return userCharSpecOption
+    }
+   
+ 
+
 // Function to prompt user for password options
 function getPasswordOptions() {
-
+  userLength()
+  userCharUp()
+  userCharLow()
+  userCharNum()
+  userCharSpec()
+  if (passOptions.types===0){
+    alert("you have to pick at least one character type")
+    getPasswordOptions
+  } else
+  alert("thanks, your password will now be randomly generated")
 }
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  var randomElement = arr[randomIndex];
+  return randomElement;
 }
+
+// // function for selecting random component
+// function getRandomProperty(obj) {
+//   var keys = Object.keys(obj);
+//   return keys[Math.floor(Math.random() * keys.length)];
+// }
+
 
 // Function to generate password with user input
 function generatePassword() {
-
+  var finalPassword = ""
+  getPasswordOptions()
+  var userOptions = Object.keys(passOptions).filter(key => passOptions[key] === true)
+  var charList = []
+  if (Object.values(userOptions).includes('charSpec')){
+    charList = charList.concat(specialCharacters)
+    if (Object.values(userOptions).includes('charNum')){
+      charList = charList.concat(numericCharacters)
+      if(Object.values(userOptions).includes('charUp')){
+        charList = charList.concat(upperCasedCharacters)
+        if(Object.values(userOptions).includes('charLow')){
+          charList = charList.concat(lowerCasedCharacters)
+      }
+    }
+  }}
+  for (var i =0; i<= passOptions.len;i++){
+    var newChar = getRandom(charList)
+    finalPassword= finalPassword.concat(newChar)
+  }
+  alert( "here is your final password \n\n"+ finalPassword)
+  return finalPassword;
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -116,3 +246,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+console.log(generatePassword())
